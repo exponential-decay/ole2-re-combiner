033781e0cf025648625884f99a93bf7abebda79d
ross-spencer <all.along.the.watchtower2001+github@gmail.com>
2015-09-22 02:49:44 +1200

    [documentation] Args.

--- ole2recombiner.py
+++ ole2recombiner.py
@@ -2,22 +2,46 @@ from JHandleOLE2Containers import ReadWriteOLE2Containers
 import argparse
 import sys
 
+def arg_check(argcount, max):
+   if argcount == max:
+      return True
+   else:
+      sys.exit("Too many arguments for option.")
+
 def main():
 
    #	Handle command line arguments for the script
    parser = argparse.ArgumentParser(description='Split or re-combine OLE2 files.')
-   parser.add_argument('--ole2', help='Optional: OLE2 file to open.', default=False)
-   parser.add_argument('--ext', help='Optional: Extension for OLE2.', default='')
-   parser.add_argument('--dir', help='Optional: Directory to combine into OLE2.', default=False)
+   parser.add_argument('--extract', help='Optional: OLE2 file to open.', default=False)
+   parser.add_argument('--combine', help='Optional: Directory to combine into OLE2.', default=False)
+   parser.add_argument('--ext', help='Optional: Extension for OLE2.', default='')   
+   parser.add_argument('--blanksummary', help='Optional: Replace SummaryInfo with a blank SummaryInfo.', default=False)
+   parser.add_argument('--fixsummary', help='Optional: Try to retrieve old SummaryInfo and fix.', default=False)
 
-   #if len(sys.argv)==1:
-      #parser.print_help()
-      #sys.exit(1)
+   if len(sys.argv)==1:
+      parser.print_help()
+      sys.exit(1)
 
-   test = ReadWriteOLE2Containers()
-   #test.writeContainer("tmp", "doc")
-   #test.extractContainer("rsdocfile.doc")
-   test.replaceSummaryInfo("rsdocfile.doc")
+   ole2class = ReadWriteOLE2Containers()
+
+   #	Parse arguments into namespace object to reference later in the script
+   global args
+   args = parser.parse_args()
+
+   if args.fixsummary:
+      if arg_check(len(sys.argv),3):
+         ole2class.replaceSummaryInfo("rsdocfile.doc")
+   elif args.blanksummary:
+      if arg_check(len(sys.argv),3):
+         ole2class.replaceSummaryInfo("rsdocfile.doc", True)      
+   elif args.extract:
+      if arg_check(len(sys.argv),3):
+         ole2class.extractContainer("rsdocfile.doc")
+   elif args.combine:
+      if not args.ext:
+         ole2class.writeContainer(args.combine, '')
+      else:
+         ole2class.writeContainer(args.combine, args.ext)   
 
 if __name__ == "__main__":      
    main()
