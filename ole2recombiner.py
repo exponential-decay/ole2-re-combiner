d0d1b154ab1d4c71e5b5f2db9cd0accee1f61a49
Ross Spencer <all.along.the.watchtower2001@gmail.com>
2019-06-19 11:25:51 +0200

    Black formatting

--- ole2recombiner.py
+++ ole2recombiner.py
@@ -2,54 +2,75 @@ from JHandleOLE2Containers import ReadWriteOLE2Containers
 import argparse
 import sys
 
+
 def arg_check(argcount, max):
-   if argcount == max:
-      return True
-   else:
-      sys.exit("Too many arguments for option.")
+    if argcount == max:
+        return True
+    else:
+        sys.exit("Too many arguments for option.")
+
 
 def main():
 
-   #	Handle command line arguments for the script
-   parser = argparse.ArgumentParser(description='Split or re-combine OLE2 files.')
-   parser.add_argument('--extract', help='Optional: OLE2 file to open.', default=False)
-   parser.add_argument('--combine', help='Optional: Directory to combine into OLE2.', default=False)
-   parser.add_argument('--ext', help='Optional: Extension for OLE2.', default='')   
-   parser.add_argument('--blanksummary', help='Optional: Replace SummaryInfo with a blank SummaryInfo.', default=False)
-   parser.add_argument('--fixsummary', help='Optional: Try to retrieve old SummaryInfo and fix.', default=False)
-   parser.add_argument('--blankdocumentsummary', help='Optional: Replace DocumentSummaryInformation with a blank DocumentSummaryInformation.', default=False)
-   parser.add_argument('--fixdocumentsummary', help='Optional: Try to retrieve old DocumentSummaryInformation and fix.', default=False)
+    # 	Handle command line arguments for the script
+    parser = argparse.ArgumentParser(description="Split or re-combine OLE2 files.")
+    parser.add_argument("--extract", help="Optional: OLE2 file to open.", default=False)
+    parser.add_argument(
+        "--combine", help="Optional: Directory to combine into OLE2.", default=False
+    )
+    parser.add_argument("--ext", help="Optional: Extension for OLE2.", default="")
+    parser.add_argument(
+        "--blanksummary",
+        help="Optional: Replace SummaryInfo with a blank SummaryInfo.",
+        default=False,
+    )
+    parser.add_argument(
+        "--fixsummary",
+        help="Optional: Try to retrieve old SummaryInfo and fix.",
+        default=False,
+    )
+    parser.add_argument(
+        "--blankdocumentsummary",
+        help="Optional: Replace DocumentSummaryInformation with a blank DocumentSummaryInformation.",
+        default=False,
+    )
+    parser.add_argument(
+        "--fixdocumentsummary",
+        help="Optional: Try to retrieve old DocumentSummaryInformation and fix.",
+        default=False,
+    )
 
-   if len(sys.argv)==1:
-      parser.print_help()
-      sys.exit(1)
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
 
-   ole2class = ReadWriteOLE2Containers()
+    ole2class = ReadWriteOLE2Containers()
 
-   #	Parse arguments into namespace object to reference later in the script
-   global args
-   args = parser.parse_args()
+    # 	Parse arguments into namespace object to reference later in the script
+    global args
+    args = parser.parse_args()
 
-   if args.fixsummary:
-      if arg_check(len(sys.argv),3):
-         ole2class.replaceSummaryInfo(args.fixsummary)
-   elif args.blanksummary:
-      if arg_check(len(sys.argv),3):
-         ole2class.replaceSummaryInfo(args.blanksummary, True) 
-   elif args.fixdocumentsummary:
-      if arg_check(len(sys.argv),3):
-         ole2class.replaceDocumentSummary(args.fixdocumentsummary)
-   elif args.blankdocumentsummary:
-      if arg_check(len(sys.argv),3):
-         ole2class.replaceDocumentSummary(args.blankdocumentsummary, True)      
-   elif args.extract:
-      if arg_check(len(sys.argv),3):
-         ole2class.extractContainer(args.extract)
-   elif args.combine:
-      if not args.ext:
-         ole2class.writeContainer(args.combine, '')
-      else:
-         ole2class.writeContainer(args.combine, args.ext)   
+    if args.fixsummary:
+        if arg_check(len(sys.argv), 3):
+            ole2class.replaceSummaryInfo(args.fixsummary)
+    elif args.blanksummary:
+        if arg_check(len(sys.argv), 3):
+            ole2class.replaceSummaryInfo(args.blanksummary, True)
+    elif args.fixdocumentsummary:
+        if arg_check(len(sys.argv), 3):
+            ole2class.replaceDocumentSummary(args.fixdocumentsummary)
+    elif args.blankdocumentsummary:
+        if arg_check(len(sys.argv), 3):
+            ole2class.replaceDocumentSummary(args.blankdocumentsummary, True)
+    elif args.extract:
+        if arg_check(len(sys.argv), 3):
+            ole2class.extractContainer(args.extract)
+    elif args.combine:
+        if not args.ext:
+            ole2class.writeContainer(args.combine, "")
+        else:
+            ole2class.writeContainer(args.combine, args.ext)
 
-if __name__ == "__main__":      
-   main()
+
+if __name__ == "__main__":
+    main()
--- uniqid.py
+++ uniqid.py
@@ -1,15 +1,16 @@
-#Source:  http://gurukhalsa.me/2011/uniqid-in-python/ 
-#Basedon: http://php.net/manual/en/function.uniqid.php 
+# Source:  http://gurukhalsa.me/2011/uniqid-in-python/
+# Basedon: http://php.net/manual/en/function.uniqid.php
 
 import string, time, math, random
- 
-def uniqid(prefix='', more_entropy=False):
+
+
+def uniqid(prefix="", more_entropy=False):
     m = time.time()
-    uniqid = '%8x%05x' %(math.floor(m),(m-math.floor(m))*1000000)
+    uniqid = "%8x%05x" % (math.floor(m), (m - math.floor(m)) * 1000000)
     if more_entropy:
         valid_chars = list(set(string.hexdigits.lower()))
-        entropy_string = ''
-        for i in range(0,10,1):
+        entropy_string = ""
+        for i in range(0, 10, 1):
             entropy_string += random.choice(valid_chars)
         uniqid = uniqid + entropy_string
     uniqid = prefix + uniqid
